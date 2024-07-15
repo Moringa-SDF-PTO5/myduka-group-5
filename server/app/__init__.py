42d0d91e6526e58cebc4d3eca5b8b826381642e3
Jospine <nzi19001@byui.edu>
2024-07-15 21:16:28 +0300

    delete app file, modify configuration

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,14 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 
-# Initialize extensions
 db = SQLAlchemy()
 migrate = Migrate()
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    ENVIRONMENT = os.getenv('APP_ENV', 'development')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def create_app():
     # Load environment variables from .env file
@@ -19,11 +13,12 @@ def create_app():
 
     app = Flask(__name__)
     
-    from .config import Config
-    app.config.from_object(Config)
+    # Configure SQLAlchemy database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
-    # Verify that the DATABASE_URL is being read correctly
-    print("Database URL:", app.config['SQLALCHEMY_DATABASE_URI'])
+    # Additional configuration
+    app.config['ENVIRONMENT'] = os.getenv('APP_ENV', 'development')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -36,7 +31,3 @@ def create_app():
         app.register_blueprint(main_blueprint)
 
     return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000)
--- server/app/config.py
+++ server/app/config.py
@@ -1,7 +1,6 @@
 import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://josephine:root@localhost:5432/inventory_db1')
-
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     ENVIRONMENT = os.getenv('APP_ENV', 'development')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
