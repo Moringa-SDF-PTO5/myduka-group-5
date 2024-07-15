8ff023d10646f52b7928523fe0d4968eb225314d
Jospine <nzi19001@byui.edu>
2024-07-15 16:52:15 +0300

    updated pytest

--- server/conftest.py
+++ server/conftest.py
@@ -1,7 +1,19 @@
 import pytest
-from app import app
+from app import create_app
 
 @pytest.fixture
 def client():
+    app = create_app()
     with app.test_client() as client:
-        yield client
+        with app.app_context():
+            yield client
+
+# 
+# 
+# import pytest
+# from app import app
+
+# @pytest.fixture
+# def client():
+#     with app.test_client() as client:
+#         yield client
