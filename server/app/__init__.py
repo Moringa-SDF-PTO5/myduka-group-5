53175c0ea39e65c5253262fe785b214010282b02
Jospine <nzi19001@byui.edu>
2024-07-16 18:16:13 +0300

    add files to branch

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,18 +1,31 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
-from app.config import Config
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     CORS(app, send_wildcard=True)
-    app.config.from_object(Config)
+
     db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
-        from app import routes, models
         db.create_all()
+
+        # Import and register blueprints
+        from .routes import main as main_blueprint
+        app.register_blueprint(main_blueprint)
 
     return app
