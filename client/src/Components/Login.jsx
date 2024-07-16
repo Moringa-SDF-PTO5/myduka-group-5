aafd2aa412576c2ad8238b6c45844cea283b76b0
Sonia <sonia@Sonias-MacBook-Pro.local>
2024-07-16 17:47:24 +0300

    login

---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './css/Login.css'
+// import { useDispatch } from 'react-redux';
+// import { login } from '../features/userSlice';
+
+const Login = () => {
+    const  [username, setUsername] = useState("");
+    const  [email, setEmail] = useState("");
+    const  [password, setPassword] = useState("");
+
+    // const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    //     dispatch(
+    //         login({
+    //         username: username,
+    //         email:email,
+    //         password:password,
+    //         loggedIn:true,
+    //     })
+    // )
+    }
+
+
+  return (
+    <div className='login'>
+        <form className='login_form' onSubmit={(e) => handleSubmit(e)}>
+        <h1>Login</h1>
+        <input
+            type='username'
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername=(e.target.value)}
+        />
+        <input
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail=(e.target.value)}
+        />
+        <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword=(e.target.value)}
+        />
+        <button type='submit' className='submit_btn'>Submit</button> 
+        </form>
+    </div>
+  )
+}
+
+export default Login
