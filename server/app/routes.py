57525c333ae5d9ec6c1bf9172514573495bc2fc1
Jospine <nzi19001@byui.edu>
2024-07-19 15:04:26 +0300

    added all user endpoints

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,6 @@
-from flask import request, jsonify, make_response,current_app as app
+from flask import request, jsonify, make_response, current_app as app
+from app import db
 from app.models import User
-
 
 
 @app.route('/')
@@ -41,15 +41,74 @@ def create_user():
             "data": None
         }), 400
 
+    new_user = User(
+        user_id=user_id,
+        username=username,
+        email=email,
+        password_hash=password_hash,
+        role=role,
+        is_active=is_active,
+        confirmed_admin=confirmed_admin
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({
+            "status": "Failed",
+            "message": "An error occurred while adding the user.",
+            "data": str(e)
+        }), 500
+
     return jsonify({
         "status": "Success",
         "message": "User created successfully.",
-        "data": {
-            "user_id": user_id,
-            "username": username,
-            "email": email,
-            "role": role,
-            "is_active": is_active,
-            "confirmed_admin": confirmed_admin
-        }
+        "data": new_user.to_dict()
     }), 201
+
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({
+            "status": "Failed",
+            "message": "User not found",
+            "data": None
+        }), 404
+
+    data = request.get_json()
+    user.username = data.get('username', user.username)
+    user.email = data.get('email', user.email)
+    user.password_hash = data.get('password_hash', user.password_hash)
+    user.role = data.get('is_active', user.is_active)
+    user.confirmed_admin = data.get('confirmed_admin', user.confirmed_admin)
+
+    db.session.commit()
+    return jsonify({
+        "status": "Success",
+        "message": "User updated successfully.",
+        "data": user.to_dict()
+    }), 200
+
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({
+            "status": "Failed",
+            "message": "User not found.",
+            "data": None
+        }), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({
+        "status": "Success",
+        "message": "User deleted successfully.",
+        "data": None
+    }), 200
