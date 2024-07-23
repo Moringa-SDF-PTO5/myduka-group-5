264ded38dcfe26f372d102dcf944da19c45eaaf5
Sfrasia <frasiakemunto@gmail.com>
2024-07-23 13:26:56 +0300

    Layout definition and products route addition

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import { store } from './store/store.js'
-import App from './App.jsx'
-import LoginPage from './features/login/page.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./store/store.js";
+import App from "./App.jsx";
+import LoginPage from "./features/login/page.jsx";
+import Layout from "./Components/Layout.jsx";
+import "./index.css";
+import ProductsPage from "./features/products/page.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/login' element={<LoginPage />} />
-                    <Route path='/dashboard' element={<App />} />
-                </Routes>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<App />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
