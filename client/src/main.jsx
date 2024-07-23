9bad092f39b111dcb66f9bd8d2d4ddff97e8008c
Sfrasia <frasiakemunto@gmail.com>
2024-07-23 10:55:59 +0300

    Routes configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import { store } from './store/store.js'
 import App from './App.jsx'
+import LoginPage from './features/login/page.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/dashboard' element={<App />} />
+                </Routes>
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 )
