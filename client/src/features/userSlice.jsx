47c07df8f325e7439359e8a7885111948579b7ab
Sfrasia <frasiakemunto@gmail.com>
2024-07-24 16:07:05 +0300

    User redux function

--- client/src/features/userSlice.jsx
+++ client/src/features/userSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 export const userSlice = createSlice({
     name: 'user',
@@ -9,7 +9,8 @@ export const userSlice = createSlice({
             userName: 'Frasia',
             role: 'admin',
             email: 'frasia.nyakundi@student.moringaschool.com'
-        }
+        },
+        users: []
     },
     reducers: {
         login: (state, action) => {
@@ -17,11 +18,30 @@ export const userSlice = createSlice({
         },
         logout: (state) => {
             state.user = null
+        },
+        fetchUsers: (state, action) => {
+            state.users = action.payload;
+        },
+        extraReducers: (builder) => {
+            builder.addCase(getUsers.fulfilled, (state, action) => {
+                state.users = action.payload
+            })
         }
     }
-})
 
-export const {login, logout} = userSlice.actions
+})
+export const getUsers = createAsyncThunk(
+    'user/getUsers',
+    async() => {
+        const response = await fetch (
+            "https://myduka-group-5-mnnj.onrender.com/users"
+        )
+        const data =  await response.json()
+        return data.data
+    } 
+)
+
+export const {login, logout, fetchUsers} = userSlice.actions
 
 export const selectUser = (state) => state.user.user 
 
