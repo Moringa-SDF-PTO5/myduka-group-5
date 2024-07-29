0c5c99eba9aa672987babe7ae9dd7039ea28966c
Barbara Tess <92345738+b-tess@users.noreply.github.com>
2024-07-29 23:45:17 +0300

    Update __init__.py
    Wrap application in CORS to check if the Access-Control-Allow-Origin header issue will resolve.

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_migrate import Migrate
 from app.config import Config
 
@@ -9,6 +10,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, send_wildcard=True)
     app.config.from_object(Config)
 
     print("Database URL:", app.config['SQLALCHEMY_DATABASE_URI'])
