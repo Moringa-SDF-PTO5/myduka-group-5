1ede0ac69b8361e0c46d9e9e825d3f568677af8f
Sfrasia <frasiakemunto@gmail.com>
2024-08-02 13:54:57 +0300

    Changes

--- client/src/features/users/page.jsx
+++ client/src/features/users/page.jsx
@@ -4,13 +4,11 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Button, Container, Typography, Grid } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
 import { fetchUsers } from '../userSlice';
-// import { getUsers } from '../userSlice';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
-// import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import EditForm from './editForm';
-// import { fetchUsers, addUser, editUser, deleteUser } from '../../actions/userActions';
+
 
 
 const USERS= [
@@ -55,15 +53,6 @@ const UsersPage = () => {
     // dispatch(getUsers());
   }, [dispatch]);
 
-//   const handleEdit = (id) => {
-//     console.log('Edit user', id);
-//     dispatch(editUser({ id, name: 'Updated Name', email: 'updated.email@example.com' }));
-//   };
-
-//   const handleDelete = (id) => {
-//     dispatch(deleteUser(id));
-//   };
-
   const columns = [
     { field: 'name', headerName: 'Name', flex: 1 },
     { field: 'email', headerName: 'Email', flex: 1 },
@@ -81,13 +70,11 @@ const UsersPage = () => {
             startIcon={<Edit />}
             color="primary"
             onClick = {() => handleClickOpen(params.row)}
-            // onClick={() => handleEdit(params.row.id)}
           >
           </Button>
           <Button
             startIcon={<Delete />}
             color="secondary"
-            // onClick={() => handleDelete(params.row.id)}
           >
           </Button>
         </div>
@@ -102,20 +89,9 @@ const UsersPage = () => {
             <Typography variant="h4" component="h1" gutterBottom>
                 Manage Clerks
             </Typography>
-            {/* <Button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 my-2 border border-gray-400 rounded shadow"> */}
             <Button  variant='contained' onClick={() => {
               setOpen(true)
             }}>Add User</Button>
-            {/* <Dialog>
-              <DialogTitle>User Details</DialogTitle>
-              <DialogContent>
-                <DialogContentText>Testing Testing</DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button variant='contained'>Submit</Button>
-                <Button variant='contained'>Cancel</Button>
-              </DialogActions>
-            </Dialog> */}
       </div>
       <Grid container spacing={3}>
         <Grid item xs={12}>
